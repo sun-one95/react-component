7306dfb557a6967c72811f2b7c94dac27c3a92f8
sun-one95 <82013784+sun-one95@users.noreply.github.com>
2021-09-08 23:30:05 +0900

    state, props 사용

--- src/App.js
+++ src/App.js
@@ -7,12 +7,28 @@ import Subject from "./Subject"
 // 지금은 html 구조를 만드는 데 사용했지만, 더 나아가서 앱이나 웹을 만들 때, 기능을 추가한 컴포넌트를 만들 수 있다.
 // 컴포넌트는 페이지를 만드는 데 있어 조각 모음 같은 것이다.
 class App extends Component {
+  // 사용자가 볼때는 App 컴포넌트에서 state 값이 있는 지 확인할 수 없다.
+  // 즉 은닉화가 가능하다.
+  // 상위 컴포넌트의 state값이 하위 컴포넌트의 props 값으로 전달 된다.
+  constructor(props) {
+    super(props)
+    this.state = {
+      subject:{title: "WEB", sub: "World Wide Web!"},
+      contents: [
+        {id: 1, title:"HTML", desc:'HTML is for information'},
+        {id: 2, title:"CSS", desc:'CSS is for design'},
+        {id: 3, title:"JavaScript", desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="APP">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
           desc="HTML is HyperText Markup Language"
